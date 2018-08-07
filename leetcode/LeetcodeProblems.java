5f0bc678c1f086a475d294a1c9b7cda99e44cb6b
William Lemens <william.lemens@gmail.com>
2018-08-06 19:54:57 -0500

    Symbolic links are a no-go.

--- leetcode/LeetcodeProblems.java
+++ leetcode/LeetcodeProblems.java
@@ -1,1 +1,27 @@
-/Users/williamlemens/Documents/eclipse-workspace/Random/src/LeetcodeProblems.java
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetcodeProblems {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+	}
+	public static int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target-nums[i];
+			if (map.containsKey(complement))
+				return new int[] {map.get(complement),i};
+			map.put(nums[i], i);
+		}
+		return null;
+    }
+	public static int[] twoSumNaive(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++)
+			for (int j = 0; j < nums.length; j++)
+				if (nums[i]+nums[j]==target)
+					return new int[] {i, j};
+        return null;
+    }
+}
