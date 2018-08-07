dce58d3bbb868ef05875f58c51e1fc3cdde52b68
William Lemens <william.lemens@gmail.com>
2018-08-06 19:52:50 -0500

    experiments with symbolic links

--- reddit/RedditProblems.java
+++ reddit/RedditProblems.java
@@ -1,17 +1,1 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class RedditProblems {
-	public static void main(String[] args) {
-		System.out.println(mostFrequentInt(new int[]{1,2,3,4,1,2,3,2,1,2,2,5}));
-	}
-	public static int mostFrequentInt(int[] arr) {
-		Map<Integer, Integer> map = new HashMap<>();
-		int maxVal = arr[0];
-		for (int i : arr) {
-			map.put(i, map.containsKey(i) ? map.get(i)+1 : 1);
-			if (map.get(i) > map.get(maxVal)) maxVal = i;
-		}
-		return maxVal;
-	}
-}
+/Users/williamlemens/Documents/eclipse-workspace/Random/src/RedditProblems.java
